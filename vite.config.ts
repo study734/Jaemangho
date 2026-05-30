5984ca372d2b79f641afaf6c359b6d8a71fde5e8
taewook <zes1357@outlook.com>
2026-05-30 14:44:25 +0900

    재망호 도메인 붙이기

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
-  base: '/jaemangho/',
+  base: '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
